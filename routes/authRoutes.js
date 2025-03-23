08ba06a07b9f957688f32bda15a7d8f28552e559
Alexandre Fernandes <alexandrefer1231@gmail.com>
2025-03-23 18:22:51 -0300

    remove log and update mongoose

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,8 +5,4 @@ const { signup, login } = require('../controllers/authControllers');
 router.post('/signup', signup);
 router.post('/login', login);
 
-router.get('/hello', (req, res) => {
-    res.json({ message: 'Hello World' });
-});
-
 module.exports = router;
