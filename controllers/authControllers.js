6e3e31716d3d06d9ad3b6cff9233aa99bf7d9244
Alexandre Fernandes <alexandrefer1231@gmail.com>
2025-03-23 21:45:09 -0300

    add some infos about the errors

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -4,7 +4,16 @@ const jwt = require('jsonwebtoken');
 exports.signup = async (req, res) => {
   try {
     const { username, email, password } = req.body;
-    console.log(username, email, password);
+    
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ 
+        error: 'Este e-mail já está cadastrado',
+        cause: 'email_duplicate',
+        details: 'Não é possível criar uma conta com um e-mail que já está em uso'
+      });
+    }
+
     const newUser = await User.create({ username, email, password });
 
     const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
@@ -13,7 +22,11 @@ exports.signup = async (req, res) => {
 
     res.status(201).json({ token });
   } catch (err) {
-    res.status(400).json({ error: err.message });
+    res.status(400).json({ 
+      error: err.message,
+      cause: err.code || 'validation_error',
+      details: err.errors ? Object.values(err.errors).map(e => e.message) : 'Erro ao criar usuário'
+    });
   }
 };
 
@@ -22,10 +35,21 @@ exports.login = async (req, res) => {
     const { email, password } = req.body;
 
     const user = await User.findOne({ email }).select('+password');
-    if (!user) throw new Error('Credenciais inválidas');
-
+    if (!user) {
+      return res.status(401).json({ 
+        error: 'Credenciais inválidas',
+        cause: 'login_error',
+        details: 'Credenciais inválidas'
+      });
+    }
     const isMatch = await user.comparePassword(password);
-    if (!isMatch) throw new Error('Credenciais inválidas');
+    if (!isMatch) {
+      return res.status(401).json({ 
+        error: 'Credenciais inválidas',
+        cause: 'login_error',
+        details: 'Credenciais inválidas'
+      });
+    }
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
       expiresIn: '1h'
@@ -33,6 +57,10 @@ exports.login = async (req, res) => {
 
     res.status(200).json({ token });
   } catch (err) {
-    res.status(401).json({ error: err.message });
+    res.status(401).json({ 
+      error: err.message,
+      cause: 'authentication_error',
+      details: 'Erro durante o processo de autenticação'
+    });
   }
 };
